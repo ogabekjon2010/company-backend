6a81f01ad84bc887a4cb5ce1afeda2d850da0e0f
shabbona2oo9 <boriyevashabbona25@gimal.com>
2025-08-07 08:54:49 +0500

    yangiladim

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Blog,Project,Company,TeamMember
 
-# Register your models here.
+admin.site.register(Blog)
+admin.site.register(Project)
+admin.site.register(Company)
+admin.site.register(TeamMember)
